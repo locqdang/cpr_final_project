411c4711949cbcd28c9c9980d60ff97cd2caf59c
Elaine Camille Javier <ecjavier@myseneca.ca>
2023-12-05 17:48:43 -0500

    tokenizing_v2

--- tokenizing.c
+++ tokenizing.c
@@ -10,9 +10,7 @@ each word with a corresponding index. */
 #define _CRT_SECURE_NO_WARNINGS
 #define BUFFER_SIZE 300
 #include "tokenizing.h"
-
-		// VI
-
+/*
 void tokenizing(void)
 {
 
@@ -52,4 +50,29 @@ void tokenizing(void)
 	
 	while (strcmp(words, "q") != 0);
 	printf("*** End of Tokenizing Words Demo ***\n\n");
+}
+*/
+
+
+/* Version 2 */
+void tokenizing(void);
+{
+	printf("*** Start of Tokenizing Phrases Demo ***\n");
+	char	phrases[BUFFER_SIZE];
+	char* nextPhrase = NULL;
+	int		phrasesCounter;
+	do {
+		printf("Type a few phrases separated by comma (q - to quit):\n");
+		fgets(phrases, BUFFER_SIZE, stdin);
+		phrases[strlen(phrases) - 1] = '\0';
+		if ((strcmp(phrases, "q") != 0)) {
+			nextPhrase = strtok(phrases, ",");
+			phrasesCounter = 1;
+			while (nextPhrase) {
+				printf("Phrase #%d is \'%s\'\n", phrasesCounter++, nextPhrase);
+				nextPhrase = strtok(NULL, ",");
+			}
+		}
+	} while (strcmp(phrases, 'q') != 0);
+	printf("*** End of Tokenizing Phrases Demo ***\n\n");
 }
