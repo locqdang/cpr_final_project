ae8954129e50f94acc551626e65aee5205167811
JONATHAN LADERA <jonathanladera11@gmail.com>
2023-12-05 14:50:53 -0500

    manipulating.c_ v2

--- manipulating.c
+++ manipulating.c
@@ -8,7 +8,9 @@
 #define BUFFER_SIZE 80
 #include "manipulating.h"
 
+
 /* Version 1 */
+/*
 void manipulating(void)
 {
     // Display a start message
@@ -48,16 +50,54 @@ void manipulating(void)
 
     // Display an end message
     printf("*** End of Concatenating Strings Demo ***\n\n");
+}*/
+
+
+
+
+/* Version 2 */
+void manipulating(void)
+{
+    printf("*** Start of Comparing Strings Demo ***\n");
+    char compare1[BUFFER_SIZE];
+    char compare2[BUFFER_SIZE];
+    int result;
+    do
+    {
+        printf("Type the 1st string to compare (q - to quit):\n");
+        fgets(compare1, BUFFER_SIZE, stdin);
+        compare1[strlen(compare1) - 1] = '\0';
+        if (strcmp(compare1, "q") != 0)
+        {
+            printf("Type the 2nd string to compare:\n");
+            fgets(compare2, BUFFER_SIZE, stdin);
+            compare2[strlen(compare2) - 1] = '\0';
+            result = strcmp(compare1, compare2);
+            if (result < 0)
+            {
+                printf("\'%s' string is less than \'%s' \n", compare1, compare2);
+            }
+            else if (result == 0)
+            {
+                printf("\'%s' string is equal to \'%s' \n", compare1, compare2);
+            }
+            else
+            {
+                printf("\'%s' string is greater than \'%s' \n", compare1, compare2);
+            }
+
+        }
+
+    } while (strcmp(compare1, "q") != 0);
+
+    {
+        printf("*** End of Comparing Strings Demo *** \n\n");
+
+    }
 }
 
 
+/* Version 3 */
 
-
-		/* Version 2 */
-
-
-
-		/* Version 3 */
-	
 
 
